d8754906f1c9ee1730b35eb196a0dfa1837ef601
ishay320 <ishay320@hotmail.com>
2023-12-15 11:04:31 +0200

    added treesitter and colored brackets

--- init.lua
+++ init.lua
@@ -14,3 +14,13 @@ require("toggleterm").setup {
 
 require('lualine').setup({
 })
+
+require('nvim-treesitter.configs').setup {
+  rainbow = {
+    enable = true,
+    -- Which query to use for finding delimiters
+    query = 'rainbow-parens',
+    -- Highlight the entire buffer all at once
+    strategy = require('ts-rainbow').strategy.global,
+  }
+}
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -60,4 +60,24 @@ require("lazy").setup({
 	},
 
 	require("config.formatter"),
+
+	{
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+		  auto_install = true,
+		  modules = {},
+		  ignore_install = {},
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+    end
+ 	},
+
+	'HiPhish/nvim-ts-rainbow2',
 })

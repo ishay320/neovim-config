48f794f912b4e152ff737df8febe9fc19dfcbd52
ishay320 <ishay320@hotmail.com>
2024-04-17 00:45:23 +0300

    Moved to kickstart

--- lua/core/colors.lua
+++
@@ -1,4 +0,0 @@
-vim.opt.termguicolors = true
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin-mocha"
--- lua/core/init.lua
+++
@@ -1,7 +0,0 @@
-require('core.setting')
-require('core.keymap')
-require('core.lazy')
-require('core.colors')
-require('core.telescope')
-require('core.lazygit')
-require('core.which-key')

af7e7a9571c676e486f5c02ad521615f15fdca36
ishay320 <ishay320@hotmail.com>
2025-03-20 16:51:25 +0200

    fix(terminal): added support for opening terminal in ghostty

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -7,8 +7,10 @@ return {
 	-- - swap between them example: 3<c-/>
 	-- - show the terminals at a click
 	opts = {
-		vim.keymap.set("n", "", "<cmd>ToggleTerm<cr>", { desc = "Terminal horizontal" }), -- <c-/>
-		vim.keymap.set("t", "", "<cmd>ToggleTerm<cr>", { desc = "Terminal horizontal" }), -- <c-/>
+		vim.keymap.set("n", "<C-/>", "<cmd>ToggleTerm<cr>", { desc = "Terminal horizontal" }),
+		vim.keymap.set("t", "<C-/>", "<cmd>ToggleTerm<cr>", { desc = "Terminal horizontal" }),
+		vim.keymap.set("n", "", "<cmd>ToggleTerm<cr>", { desc = "Terminal horizontal" }), -- tmux && gnome terminal
+		vim.keymap.set("t", "", "<cmd>ToggleTerm<cr>", { desc = "Terminal horizontal" }), -- tmux && gnome terminal
 		vim.keymap.set("t", "<C-Up>", "<cmd>resize +1<cr>", { desc = "Terminal resize bigger" }),
 		vim.keymap.set("t", "<C-Down>", "<cmd>resize -1<cr>", { desc = "Terminal resize smaller" }),
 		vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]]),

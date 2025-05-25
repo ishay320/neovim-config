66e3d220d48f4b3618ac55363bdfc511adaac127
ishay320 <ishay320@hotmail.com>
2025-05-25 23:42:33 +0300

    feat(copilot): added to lualine and fixed the toggle

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,24 +1,49 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({
-				suggestion = { enabled = false },
-				panel = { enabled = false },
-			})
-			-- on <spce>tc, toggle copilot
-			vim.keymap.set("n", "<space>tc", function()
-				require("copilot.panel").toggle()
-			end, { desc = "Toggle Copilot Panel" })
-		end,
-	},
-	{
-		"zbirenbaum/copilot-cmp",
-		after = { "copilot.lua" },
-		config = function()
-			require("copilot_cmp").setup()
-		end,
-	},
+	"zbirenbaum/copilot.lua",
+	build = ":Copilot auth",
+	config = function()
+		require("copilot").setup({
+			panel = {
+				enabled = false,
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				debounce = 75,
+				keymap = {
+					accept = "<Tab>",
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-e>",
+				},
+			},
+			logger = {
+				file = vim.fn.stdpath("log") .. "/copilot-lua.log",
+				file_log_level = vim.log.levels.OFF,
+				print_log_level = vim.log.levels.ERROR,
+				trace_lsp = "off", -- "off" | "messages" | "verbose"
+				trace_lsp_progress = false,
+				log_lsp_messages = false,
+			},
+		})
+
+		-- Start with Copilot disabled
+		vim.cmd("Copilot disable")
+
+		-- Toggle Copilot suggestion with <space>tc
+		local copilot_enabled = false
+		vim.keymap.set("n", "<space>tc", function()
+			if copilot_enabled then
+				-- Disable Copilot suggestions
+				vim.cmd("Copilot disable")
+				copilot_enabled = false
+				print("Copilot disabled")
+			else
+				-- Enable Copilot suggestions
+				vim.cmd("Copilot enable")
+				copilot_enabled = true
+				print("Copilot enabled")
+			end
+		end, { desc = "Toggle Copilot Suggestion" })
+	end,
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -71,8 +71,46 @@ return {
 	-- show the line with all the data down there
 	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {},
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"AndreM222/copilot-lualine",
+		},
+		config = {
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {},
+				always_divide_middle = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {
+					"branch",
+					"diff",
+					{
+						"diagnostics",
+						sources = { "nvim_diagnostic" },
+						symbols = { error = " ", warn = " ", info = " ", hint = " " },
+					},
+				},
+				lualine_c = { "filename" },
+				lualine_x = { "copilot", "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			extensions = {},
+		},
 	},
 
 	-- color the open&close parentheses

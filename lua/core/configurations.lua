9b05ec9900e7b99250d894e4743c3616078f1e07
Ishay Trattner <ishayt@g2-sys.com>
2025-06-30 12:05:03 +0300

    feat(terminal): in windows added configs that make the terminal PS

--- lua/core/configurations.lua
+++ lua/core/configurations.lua
@@ -94,3 +94,14 @@ end
 -- Improve netrw file explorer behavior
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
+
+-- Terminal
+if vim.fn.has("win32") == 1 then
+	vim.o.shell = "powershell"
+	vim.o.shellcmdflag =
+		"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+	vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+	vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+	vim.o.shellquote = ""
+	vim.o.shellxquote = ""
+end

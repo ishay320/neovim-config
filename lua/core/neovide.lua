194c5ed7046e7ace23d573a4641e7f83e28e29ef
Ishay Trattner <ishayt@g2-sys.com>
2025-08-07 08:54:38 +0300

    feat(neovide): added keybinds to neovide

--- lua/core/neovide.lua
+++ lua/core/neovide.lua
@@ -1,3 +1,20 @@
 if vim.g.neovide then
 	vim.o.guifont = "FiraMono Nerd Font Mono:h12"
+	vim.api.nvim_set_keymap("v", "<sc-c>", '"+y', { noremap = true })
+	vim.api.nvim_set_keymap("v", "<sc-v>", '"+p', { noremap = true })
+	vim.api.nvim_set_keymap("n", "<sc-v>", '"+p', { noremap = true }) -- normal
+	vim.api.nvim_set_keymap("c", "<sc-v>", "<C-R>0", { noremap = true }) -- command
+	vim.api.nvim_set_keymap("i", "<sc-v>", '<ESC>"+pi', { noremap = true }) -- insert
+	vim.api.nvim_set_keymap("t", "<sc-v>", '<C-\\><C-n>"+pi', { noremap = true }) -- terminal
+
+	vim.g.neovide_scale_factor = 1.0
+	local change_scale_factor = function(delta)
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+	end
+	vim.keymap.set("n", "<C-=>", function()
+		change_scale_factor(1.1)
+	end)
+	vim.keymap.set("n", "<C-->", function()
+		change_scale_factor(1 / 1.1)
+	end)
 end

1e44097d69ae627d252aecd8149c046025044f82
ishay320 <ishay320@hotmail.com>
2026-01-23 01:20:11 +0200

    fix(image): fixed notification of the image plugin

--- lua/plugins/ui/image.lua
+++ lua/plugins/ui/image.lua
@@ -7,17 +7,20 @@ return {
 		-- Check if magick command exists
 		local magick_installed = vim.fn.executable("magick") == 1 or vim.fn.executable("convert") == 1
 
-		if not magick_installed then
-			vim.notify(
-				"ImageMagick not found. Please install it to use image.nvim\n"
-					.. "Ubuntu/Debian: sudo apt-get install libmagickwand-dev\n"
-					.. "Arch: sudo pacman -S imagemagick\n",
-				vim.log.levels.WARN
-			)
-			return false
+		-- Notify only once if not installed
+		if not magick_installed and not vim.g.image_nvim_warning_shown then
+			vim.g.image_nvim_warning_shown = true
+			vim.schedule(function()
+				vim.notify(
+					"ImageMagick not found. Please install it to use image.nvim\n"
+						.. "Ubuntu/Debian: sudo apt-get install libmagickwand-dev\n"
+						.. "Arch: sudo pacman -S imagemagick",
+					vim.log.levels.WARN
+				)
+			end)
 		end
 
-		return true
+		return magick_installed
 	end,
 	opts = {},
 }
--- lua/plugins/ui/nvim-notify.lua
+++ lua/plugins/ui/nvim-notify.lua
@@ -1,5 +1,6 @@
 return {
 	"rcarriga/nvim-notify",
+	priority = 100,
 	opts = {},
 	config = function()
 		vim.notify = vim.schedule_wrap(require("notify").notify)
